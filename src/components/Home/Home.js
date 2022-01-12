da18932b45d9f06a3389f803f2924e3f01e40c60
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-01-12 14:46:40 +0200

    adjusted working hours

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -45,15 +45,13 @@ export default function Home() {
 
                                     <h2 className="mb-4 text-white" data-aos="fade-up" data-aos-delay="500">Working hours</h2>
 
-                                    <strong className="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Monday - Friday</strong>
+                                    <strong className="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Monday - Saturday</strong>
 
-                                    <p data-aos="fade-up" data-aos-delay="800">7:00 AM - 10:00 PM</p>
+                                    <p data-aos="fade-up" data-aos-delay="800">By appointment</p>
 
-                                    <strong className="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Saturday</strong>
+                                    <strong className="d-block" data-aos="fade-up" data-aos-delay="600">Sunday</strong>
 
-                                    <p data-aos="fade-up" data-aos-delay="800">6:00 AM - 4:00 PM</p>
-
-                                    <strong className="d-block" data-aos="fade-up" data-aos-delay="600">Sunday : Closed</strong>
+                                    <p data-aos="fade-up" data-aos-delay="800">By appointment</p>
                                 </div>
                             </div>
                         </div>
