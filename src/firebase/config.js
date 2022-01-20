fbd3832b89686d738d5452da4b700ec32b840d07
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-01-20 09:18:32 +0200

    added timestamp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -14,7 +14,8 @@ const firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  const projectStorage = firebase.storage()
-  const projectFirestore = firebase.firestore()
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-  export { projectStorage, projectFirestore } 
+  export { projectStorage, projectFirestore, timestamp } 
