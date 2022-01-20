2e858804f6423349cdd96557cd746ed2cbf13af6
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-01-20 09:59:43 +0200

    added imagegrid

--- src/components/Gallery/ImageGrid.js
+++ src/components/Gallery/ImageGrid.js
@@ -1,7 +1,15 @@
+import useFirestore from '../../hooks/useFirestore';
+
 export default function ImageGrid() {
+    const {docs} = useFirestore('images')
+
     return (
         <div className="img-grid">
-            images
+            {docs && docs.map(doc => (
+                <div className='img-wrap' key={doc.id}>
+                    <img src={doc.url} alt='uploaded pic'/>
+                </div>
+            ))}
         </div>
     )
 }
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { projectFirestore, projectStorage } from "../firebase/config";
+import { projectFirestore} from "../firebase/config";
 
 const useFirestore = (collection) => {
     const [docs, setDocs] = useState([])
 
     useEffect(() => {
-        const unsub = projectStorage.collection(collection)
+        const unsub = projectFirestore.collection(collection)
             .orderBy('createdAt', 'desc')
             .onSnapshot((snap)=> {
                 let documents = [];
@@ -16,8 +16,10 @@ const useFirestore = (collection) => {
             })
 
             return () => unsub()
-            
+
     }, [collection])
 
     return {docs}
 }
+
+export default useFirestore;
